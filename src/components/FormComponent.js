968cad347fd40f90cdf4609d888e127f94cc4f9c
Akash28021997 <akash.pawar@rkdewan.com>
2024-08-28 18:16:00 +0530

    changes

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,45 +1,55 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, Button, FormControl, FormLabel, Input, Heading, Text, Flex, Image } from '@chakra-ui/react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  Text,
+  Flex,
+  Image,
+} from "@chakra-ui/react";
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    number: '',
-    photos: []
+    name: "",
+    number: "",
+    photos: [],
   });
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handlePhotoUpload = (e) => {
     setFormData({
       ...formData,
-      photos: Array.from(e.target.files)
+      photos: Array.from(e.target.files),
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/preview', { state: formData });
+    navigate("/preview", { state: formData });
   };
 
   return (
     <Box minH="100vh" display="flex" flexDirection="column">
       {/* Header */}
-      <Box
-        bg="blue.500"
-        color="white"
-        py={4}
-        position="relative"
-        width="100%"
-      >
-        <Flex align="center" px={5} maxW="1200px" mx="auto">
+      <Box bg="blue.500" color="white" py={4} position="relative" width="100%">
+        <Flex
+          align="center"
+          px={5}
+          maxW="1200px"
+          mx="auto"
+          justify="space-between"
+        >
           <Box
             bg="white"
             borderRadius="md"
@@ -60,43 +70,41 @@ const FormComponent = () => {
 
       {/* Main Form */}
       <Box p={5} maxW="600px" mx="auto" flex="1">
-        <Heading mb={5} textAlign="center">Enter Your Details</Heading>
+        <Heading mb={5} textAlign="center">
+          Enter Your Details
+        </Heading>
         <form onSubmit={handleSubmit}>
           <FormControl mb={3} isRequired>
             <FormLabel>Name:</FormLabel>
-            <Input 
-              type="text" 
-              name="name" 
-              value={formData.name} 
-              onChange={handleInputChange} 
+            <Input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
               isFullWidth
             />
           </FormControl>
           <FormControl mb={3} isRequired>
-            <FormLabel>Number:</FormLabel>
-            <Input 
-              type="text" 
-              name="number" 
-              value={formData.number} 
-              onChange={handleInputChange} 
+            <FormLabel>Application Number:</FormLabel>
+            <Input
+              type="text"
+              name="number"
+              value={formData.number}
+              onChange={handleInputChange}
               isFullWidth
             />
           </FormControl>
           <FormControl mb={3} isRequired>
             <FormLabel>Upload Photos:</FormLabel>
-            <Input 
-              type="file" 
-              multiple 
-              accept="image/jpeg, image/png" 
-              onChange={handlePhotoUpload} 
+            <Input
+              type="file"
+              multiple
+              accept="image/jpeg, image/png"
+              onChange={handlePhotoUpload}
               isFullWidth
             />
           </FormControl>
-          <Button 
-            type="submit" 
-            colorScheme="blue" 
-            width="full"
-          >
+          <Button type="submit" colorScheme="blue" width="full">
             Preview
           </Button>
         </form>
@@ -112,7 +120,7 @@ const FormComponent = () => {
         bottom={0}
         width="100%"
       >
-        <Text>All Rights Reserved 2024</Text>
+        <Text>All Rights Reserved @ 2024</Text>
       </Box>
     </Box>
   );
